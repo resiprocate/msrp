a0b69c10caf83299e6553a0bc92e220bb17e665a
Cullen Jennings <fluffy@cisco.com>
2005-06-11 21:38:00 +0000

    formatting
    git-svn-id: https://svn.resiprocate.org/rep/msrp/main@52 7dbb674c-5cea-0310-bdab-b43d8b1e1707

--- src/Connection.h
+++ src/Connection.h
@@ -5,49 +5,55 @@
 
 namespace msrp
 {
-  class Address;
-  class Connection
-  {
-    public:
+class Address;
+class Stack;
+
+class Connection
+{
+   public:
       friend class ConnectionGroup;
-
+      
       virtual ~Connection();
-
+      
       virtual bool connect(Address &remoteAddress) = 0;
       virtual void close() = 0;
-
-      virtual int read(char *data, size_t count) = 0;
-
+      
       virtual int write(char *data, size_t count) = 0;
-
-    protected:
-      Connection(stack *);
-
+      
+      void processReads();
+      
+   private:        
+      virtual int read(char *data, size_t count) = 0;
+      
+   protected:
+      Connection(Stack *);
+      
       /**
-        This descriptor must be usable in e.g. a select
-        (or similar) statement. There is no requirement that
-        calling read() on this descriptor will actually have
-        any useful effect. For descriptor-based connections,
-        this can just be the actual descriptor for the
-        underlying socket. For any connections that dont have
-        such a descriptor, the connection object is responsbile
-        for making sure that this descriptor will be flagged
-        as having data available whenever data on the corresponding
-        connection becomes available. In general, the easiest way
-        to do this is to set up a pipe (e.g. using "pipe()") and
-        writing a single byte into the pipe when data is available.
-
-        I have verified that this scheme should work under Windows
-        in addition to actual Posix-compliant operating systems.
-
-        I also think this *should* work transparently with libevent.
-       */
+         This descriptor must be usable in e.g. a select
+         (or similar) statement. There is no requirement that
+         calling read() on this descriptor will actually have
+         any useful effect. For descriptor-based connections,
+         this can just be the actual descriptor for the
+         underlying socket. For any connections that dont have
+         such a descriptor, the connection object is responsbile
+         for making sure that this descriptor will be flagged
+         as having data available whenever data on the corresponding
+         connection becomes available. In general, the easiest way
+         to do this is to set up a pipe (e.g. using "pipe()") and
+         writing a single byte into the pipe when data is available.
+         
+         I have verified that this scheme should work under Windows
+         in addition to actual Posix-compliant operating systems.
+         
+         I also think this *should* work transparently with libevent.
+      */
       int mDescriptor;
       
       stack *mStack;
       
       Address *mLocalAddress;
       Address *mRemoteAddress;
-  };
+};
+
 }
 #endif
